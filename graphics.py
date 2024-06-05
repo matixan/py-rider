8f8a1045341d42cdeb3ee42328094673f6798c7b
matixan <matixan02@gmail.com>
2024-06-05 04:22:35 +0200

    update

--- graphics.py
+++ graphics.py
@@ -13,64 +13,66 @@ GREY = (128, 128, 128)
 WHITE = (255, 255, 255)
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self, x, y, color, wheels_color=BLACK):
+    def __init__(self, x, y, color, opponent=False):
         super().__init__()
+        self.bot = opponent
         self.x = x
         self.y = y
+        self.distance = 0
         self.color = color
-        self.width = 100
-        self.height = 25
-        self.cabin_width = 50
-        self.cabin_height = 10
-        self.wheels_color = wheels_color
-        self.max_speed = 50 # Maximum speed value in km/h
-        self.max_acc = 0.5 # Maximum acceleration value in m/s^2
+        self.width = 50
+        self.height = 100
+        self.max_speed = 150 # Maximum speed value in km/h
         self.speed = 0
         self.acc = 0
         self.weight = 1000 # Weight of the car in kg
-        self.traction = 0.70 # Traction coefficient in range 0-1
-        self.engine = Engine(100)
+        if self.bot: self.engine = Engine(80) # you can beat him
+        else: self.engine = Engine(100)
 
     def draw(self, surface):
-        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height), border_radius=10)
-        pygame.draw.circle(surface, self.wheels_color, (self.x + 10, self.y + self.height), 10)
-        pygame.draw.circle(surface, self.wheels_color, (self.x + self.width - 10, self.y + self.height), 10)
-        pygame.draw.rect(surface, self.color, (self.x + (self.width - self.cabin_width) // 2, self.y - self.height + 5, self.cabin_width, self.cabin_height), border_radius=5)
-        pygame.draw.polygon(surface, self.color, [(self.x + (self.width - self.cabin_width) // 2, self.y - self.height + 5), (self.x + (self.width + self.cabin_width) // 2, self.y - self.height + 5), (self.x + self.width, self.y)])
-    
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height), border_radius=8)
+
     def gearUp(self):
         if self.engine.gear < 6:
             self.engine.gear += 1
             print("Gear up: ", self.engine.gear)
+            if self.engine.gear != 1:
+                self.engine.current_rpm /= 2
     def gearDown(self):
-        if self.engine.gear > 1:
+        if self.engine.gear >= 1:
             self.engine.gear -= 1
             print("Gear down: ", self.engine.gear)
-
-    def apply_accelerate(self):
-        self.acc += 0.1/(self.speed+1)
-        if self.acc > self.max_acc:
-            self.acc = self.max_acc
+            if self.engine.gear != 1:
+                self.engine.current_rpm *= 2
 
     def update_phisics(self):
-        self.speed += self.acc
-        if self.speed < 0:
-            self.speed = 0
-        if self.engine.gear != 0:
-            self.engine.current_rpm = self.speed * 1000 / (0.6 * math.pi * 0.3)
+        # Calculate acceleration force
+        if self.engine.gear == 0:
+            self.engine.current_rpm = self.engine.idle_rpm
+        else:
+            # Power increases with RPM up to a point, then decreases based on the power curve
+            power = self.engine.power_curve * self.engine.power
+            print("Power: ", power)
+            force = power * 0.7 # 70% of the power is used to move the car
+            self.acc = force / self.weight
+            # Calculate RPM
+            self.engine.current_rpm = self.engine.current_rpm + self.acc * 1000
+            if self.engine.current_rpm > self.engine.max_rpm:
+                # Engine is limited to the maximum RPM, so the speed is not increasing anymore
+                self.engine.current_rpm = self.engine.max_rpm
+                self.speed = self.engine.max_rpm * self.engine.gear * 0.5 / 1000
+            # Calculate speed
+            if self.speed < 0:
+                self.speed = 0
+            if self.speed > self.max_speed:
+                self.speed = self.max_speed
+            # Calculate distance
+            self.distance += self.speed / 3600 # 1 hour = 3600 seconds
+            
+        print("Speed: ", self.speed, "Distance: ", self.distance, "RPM: ", self.engine.current_rpm, "Acc: ", self.acc)
 
-        if self.speed >= self.max_speed:
-            self.speed = self.max_speed
-        elif self.speed <= -self.max_speed+20:
-            self.speed = -self.max_speed+20
-    def engine_idle(self):
-        # when engine is neither accelerating nor braking, it should decelerate slightly
-        if self.acc != 0:
-            self.acc -= 0.01
-            if self.acc < 0:
-                self.acc = 0
-    def update_position(self):
-        self.x += self.speed/3.6 # speed is in km/h, we need to convert it to m/s
+
+        
 
 class Engine(pygame.sprite.Sprite):
     def __init__(self, power):
@@ -78,81 +80,157 @@ class Engine(pygame.sprite.Sprite):
         self.gear=0
         self.power = power # Power of the engine in KW
         self.torque = 300 # Torque of the engine in Nm
-        self.current_rpm = 0 # Current revolutions per minute
         self.idle_rpm = 900 # Revolutions per minute
+        self.current_rpm = self.idle_rpm # Current revolutions per minute
         self.max_rpm = 8000 # Maximum revolutions per minute
+        self.power_curve = self.calculate_power_curve()
 
+    def calculate_power_curve(self):
+        function = 0.5 * self.current_rpm/1000 - 0.06 * (self.current_rpm/1000)**2 -0.2
+        return function
 class Road(pygame.sprite.Sprite):
-    def __init__(self, width, x, y, color):
-        self.x = x # metry
-        self.y = y # offset
-        self.terrain_color = color
+    def __init__(self, width, height):
+        super().__init__()
         self.width = width
-        self.alpha = 0.005  # Decreased alpha value for smoother slopes
-        self.shape = self.generate_shape()
-
-    def generate_shape(self):
-        shape = []
-        for i in range(self.width):
-            shape.append(self.y * math.sin(self.alpha * i + self.x) + self.y)
-        return shape
-    
-    def draw(self, surface, x, y):
-        pygame.draw.polygon(surface, self.terrain_color, [(0, self.y), (0, self.shape[0]), (self.width, self.shape[-1]), (self.width, self.y)])
-
+        self.height = height
+    # from bottom to top
+    def draw(self, surface, distance):
+        # road is grey
+        pygame.draw.rect(surface, GREY, (0, 0, self.width, self.height))
+        # road lines are white and dashed, lines are moving from top to bottom
+        for i in range(-50, self.height + 50, 100):
+            pygame.draw.line(surface, WHITE, (self.width//2, i+distance%100), (self.width//2, i+50+distance%100), 5)
+        # on the left and right side of the road there are green fields
+        pygame.draw.rect(surface, GREEN, (0, 0, self.width//2-100, self.height))
+        pygame.draw.rect(surface, GREEN, (self.width//2+100, 0, self.width//2-100, self.height))
+        # at the end of the road there is a finish line
+        if distance > 1000:
+            pygame.draw.rect(surface, WHITE, (self.width//2-100, 0, 200, 50))
+            font = pygame.font.Font(None, 36)
+            text = font.render("FINISH", True, BLACK)
+            text_rect = text.get_rect(center=(self.width//2, 25))
+            surface.blit(text, text_rect)
 
 class Dashboard(pygame.sprite.Sprite):
-    def __init__(self, x, y, color):
+    def __init__(self, x, y):
         super().__init__()
-        self.x = x
+        self.x = x-225
         self.y = y
-        self.color = color
         self.radius = 100
-        self.rpm = 0
-        self.speed = 23
+        self.old_rpm = 0
         self.max_rpm = 8000
         self.max_speed = 150
 
-    def draw(self, surface, rpm, speed):
+    def draw(self, surface, rpm, speed, gear):
+        # draw speedometer on the left side and tachometer on the right side
         pygame.draw.circle(surface, GREY, (self.x, self.y), self.radius)
-        pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius, 5)
-        pygame.draw.circle(surface, GREY, (self.x+250, self.y), self.radius)
-        pygame.draw.circle(surface, self.color, (self.x+250, self.y), self.radius, 5)
-        self.draw_needle(surface, self.x, self.y, self.rpm, self.max_rpm, self.radius * 0.8, self.radius * 0.05)
-        self.draw_needle(surface, self.x+250, self.y, self.speed, self.max_speed, self.radius * 0.6, self.radius * 0.05)
-        self.draw_label(surface, "RPM", self.x, self.y+75)
-        self.draw_label(surface, "SPEED", self.x+250, self.y+75)
-        self.draw_value(surface, round(rpm), self.x, self.y-75)
-        self.draw_value(surface, round(speed), self.x+250, self.y-75)
-        self.draw_axis(surface, self.x, self.y, self.radius, 10, -45, 225)
-        self.draw_axis(surface, self.x+250, self.y, self.radius, 10, -45, 225)
+        pygame.draw.circle(surface, BLACK, (self.x, self.y), self.radius, 5)
+        pygame.draw.circle(surface, GREY, (self.x+450, self.y), self.radius)
+        # draw not optimal rpm range in red from 6000 to 8000 using arc
+        pygame.draw.arc(surface, RED, (self.x+450-self.radius, self.y-self.radius, 200, 200), math.radians(40), math.radians(80), 40)
+        pygame.draw.arc(surface, GREEN, (self.x+450-self.radius, self.y-self.radius, 200, 200), math.radians(80), math.radians(110), 40)
+        pygame.draw.circle(surface, BLACK, (self.x+450, self.y), self.radius, 5)
+        # draw small labels for each 1000 rpm and 20 km/h
+        for i in range(0, 9):
+            angle = (i/8) * 180-225
+            reduced_radius = self.radius * 0.8  # Reduce the radius to draw inside the circle
+            font = pygame.font.Font(None, 24)
 
-
-    def draw_needle(self, surface, x, y, value, max_value, length, thickness):
-        angle = 180 - value / max_value * 180
-        end_x = x + length * math.cos(math.radians(angle))
-        end_y = y - length * math.sin(math.radians(angle))
-        pygame.draw.line(surface, WHITE, (x, y), (end_x, end_y), int(thickness))
-        surface.blit(surface, (end_x, end_y))
-
-    def draw_label(self, surface, label, x, y):
-        font = pygame.font.Font(None, 20)
-        text = font.render(label, True, WHITE)
-        text_rect = text.get_rect(center=(x, y))
+            x = self.x + reduced_radius * math.cos(math.radians(angle))
+            y = self.y + reduced_radius * math.sin(math.radians(angle))
+            text = font.render(str(i*20), True, BLACK)
+            text_rect = text.get_rect(center=(int(x), int(y)))
+            surface.blit(text, text_rect)
+                
+            x = self.x + 450 + reduced_radius * math.cos(math.radians(angle))
+            y = self.y + reduced_radius * math.sin(math.radians(angle))
+            text = font.render(str(i*1), True, BLACK)
+            text_rect = text.get_rect(center=(int(x), int(y)))
+            surface.blit(text, text_rect)
+        # draw labels
+        font = pygame.font.Font(None, 36)
+        text = font.render("Speed", True, BLACK)
+        text_rect = text.get_rect(center=(self.x, self.y-25))
         surface.blit(text, text_rect)
-
-    def draw_value(self, surface, value, x, y):
-        font = pygame.font.Font(None, 20)
-        text = font.render(str(value), True, WHITE)
-        text_rect = text.get_rect(center=(x, y))
+        # draw speedometer needle
+        angle = (speed/self.max_speed) * 180-225 # simple fix for the needle starting position
+        x = self.x
+        y = self.y
+        x1 = x + self.radius * math.cos(math.radians(angle))
+        y1 = y + self.radius * math.sin(math.radians(angle))
+        pygame.draw.line(surface, WHITE, (x, y), (x1, y1), 5)
+        # draw gear indicator
+        text = font.render("Gear: " + str(gear), True, BLACK)
+        text_rect = text.get_rect(center=(self.x+450, self.y+25))
         surface.blit(text, text_rect)
+        # draw tachometer needle
+        text = font.render("RPM", True, BLACK)
+        text_rect = text.get_rect(center=(self.x+450, self.y-25))
+        surface.blit(text, text_rect)
+        # smooth needle movement
+        difference = rpm - self.old_rpm
+        if difference > 200:
+            difference = 200
+        elif difference < -200:
+            difference = -200
+        self.old_rpm += difference
+        angle = (self.old_rpm/self.max_rpm) * 180-225
+        x = self.x+450
+        y = self.y
+        x1 = x + self.radius * math.cos(math.radians(angle))
+        y1 = y + self.radius * math.sin(math.radians(angle))
+        pygame.draw.line(surface, WHITE, (x, y), (x1, y1), 5)
 
-    # draws small lines around perimeter of the circle
-    def draw_axis(self, surface, x, y, radius, num_lines, min_phi, max_phi):
-        for i in range(num_lines):
-            phi = min_phi + i * (max_phi - min_phi) / num_lines
-            start_x = x + (radius - 10) * math.cos(math.radians(phi))
-            start_y = y - (radius - 10) * math.sin(math.radians(phi))
-            end_x = x + radius * math.cos(math.radians(phi))
-            end_y = y - radius * math.sin(math.radians(phi))
-            pygame.draw.line(surface, WHITE, (start_x, start_y), (end_x, end_y), 2)
+class StartingLights(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x-125
+        self.y = y
+        self.radius = 20
+        self.lights = [RED, RED, RED, RED, RED]
+        self.delay = random.randint(2, 5) * 100
+        self.last_time = 0
+        self.current_time = 0
+        self.state = 0
+
+    def draw(self, surface):
+        #draw starting lights background
+        pygame.draw.rect(surface, BLACK, (self.x, self.y-25, 5*50, 50))
+        # draw 5 lights
+        for i in range(5):
+            pygame.draw.circle(surface, self.lights[i], (self.x+i*50+25, self.y), self.radius)
+        if self.state == 0:
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > self.delay:
+                self.last_time = self.current_time
+                self.state += 1
+        elif self.state == 1:
+            self.lights[0] = GREEN
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > 1000:
+                self.last_time = self.current_time
+                self.state += 1
+        elif self.state == 2:
+            self.lights[1] = GREEN
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > 1000:
+                self.last_time = self.current_time
+                self.state += 1
+        elif self.state == 3:
+            self.lights[2] = GREEN
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > 1000:
+                self.last_time = self.current_time
+                self.state += 1
+        elif self.state == 4:
+            self.lights[3] = GREEN
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > 1000:
+                self.last_time = self.current_time
+                self.state += 1
+        elif self.state == 5:
+            self.lights[4] = GREEN
+            self.current_time = pygame.time.get_ticks()
+            if self.current_time - self.last_time > 1000:
+                self.last_time = self.current_time
+                self.state += 1
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-#This is a simple game where you are late to your Politechnika Poznańska exams.
+#This is a simple game where you are late to your Politechnika Poznańska classes.
+#You have to drive as fast as you can to get there on time before the professor.
+#You can change gears with UP and DOWN arrows.
+#You are racing against professor on a straight 1/4 mile road.
+#You can see your speed, RPM, gear and distance to the destination on the dashboard.
+#You can pause the game by pressing P.
+#if you loose, you are late to the classes and you have to try again next time.
+
+#force download of pygame
+try:
+    import pygame
+except ImportError:
+    import os
+    os.system("pip install pygame")
+    import pygame
 
 # this is game engine importing
-import pygame, sys, random
+import sys, random, time
 from pygame.locals import *
 pygame.init()
 
@@ -19,45 +33,81 @@ FramePerSec = pygame.time.Clock()
 
 # create window
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 640
+SCREEN_HEIGHT = 800
 MENU_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 menu_window = pygame.display.set_mode(MENU_SIZE)
 menu_window.fill(WHITE)
 pygame.display.set_caption("PyRider - Politechnika Poznańska Edition")
 icon = pygame.image.load('data/PP_logo_RGB.png')
 pygame.display.set_icon(icon)
-gamePaused = False
 
-# create car
-car = graphics.Car(175, 200, RED)
+# create welcome screen
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render('Welcome to PyRider - Politechnika Poznańska Edition', True, (0, 0, 0))
+text_rect = textsurface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 50))
+menu_window.blit(textsurface, text_rect)
+
+textsurface = myfont.render('Press any key to start', True, (0, 0, 0))
+text_rect = textsurface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+menu_window.blit(textsurface, text_rect)
+pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            break
+        if event.type == QUIT:
+            pygame.quit()      
+            sys.exit()
+    if event.type == pygame.KEYDOWN:
+        break
+    FramePerSec.tick(FPS)
 
 # create road
-road = graphics.Road(SCREEN_WIDTH,0, 0, GREEN)
+road = graphics.Road(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-# create dashboard on the bottom of the screen
-dashboard = graphics.Dashboard(500, 600, BLACK)
+# create cars
+car = graphics.Car(SCREEN_WIDTH/2+25, SCREEN_HEIGHT-150, BLUE, False)
+car_oponent = graphics.Car(SCREEN_WIDTH/2-75, SCREEN_HEIGHT-150, RED, True)
+
+#create countdown lights with random 2-5s delay
+starting_lights = graphics.StartingLights(SCREEN_WIDTH/2, 100)
+
+# calculate dashboard position
+dashboard = graphics.Dashboard(SCREEN_WIDTH/2, SCREEN_HEIGHT-120)
 lastTimeGearChange = 0
+FalseStart = False
+
 while True:   
     lastTimeGearChange += 1  
     for event in pygame.event.get():              
         if event.type == QUIT:
             pygame.quit()      
             sys.exit()
-    #handling pauzy
-    while gamePaused:
+    # countdown lights
+    while starting_lights.state < 5:
+        road.draw(menu_window, car.x)
+        car.draw(menu_window)
+        car_oponent.draw(menu_window)
+        dashboard.draw(menu_window, car.engine.current_rpm, car.speed, car.engine.gear)
+        starting_lights.draw(menu_window)
         pygame.display.update()
         FramePerSec.tick(FPS)
-        pygame.draw.label(menu_window)
+        # check for false start
         for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()      
+                sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_p:
-                    gamePaused = False
-                    print("Game unpaused")
-
+                if event.key == pygame.K_UP:
+                    print("False start!")
+                    FalseStart = True
+                    break
+        if FalseStart:
+            break
+    if FalseStart:
+        break
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RIGHT:
-            car.apply_accelerate()
-            print("Accelerating, acc: ", round(car.acc, 4), "speed: ", round(car.speed, 4), "rpm: ", round(car.engine.current_rpm,4))
         if event.key == pygame.K_UP:
             if lastTimeGearChange > 10:
                 car.gearUp()
@@ -66,18 +116,13 @@ while True:
             if lastTimeGearChange > 10:
                 car.gearDown()
                 lastTimeGearChange = 0
-        if event.key == pygame.K_p:
-            gamePaused = True
-            print("Game paused")
 
-    car.engine_idle()
-    print("Idle, acc: ", round(car.acc, 4), "speed: ", round(car.speed, 4), "rpm: ", round(car.engine.current_rpm,4))
-    menu_window.fill(WHITE)
+    road.draw(menu_window, car.x)
     car.update_phisics()
-    car.update_position()
+    car_oponent.update_phisics()
     car.draw(menu_window)
-    road.draw(menu_window, car.x, car.y)
-    dashboard.draw(menu_window, car.engine.current_rpm, car.speed)
+    car_oponent.draw(menu_window)
+    dashboard.draw(menu_window, car.engine.current_rpm, car.speed, car.engine.gear)
     # print car position as remaining distance to the destination
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 25)
@@ -85,3 +130,29 @@ while True:
     menu_window.blit(textsurface,(0,0))
     pygame.display.update()
     FramePerSec.tick(FPS)
+
+if FalseStart:
+    pygame.draw.rect(menu_window, WHITE, (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("You are late to the classes!")
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('False start!', True, (0, 0, 0))
+    text_rect = textsurface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 50))
+    menu_window.blit(textsurface, text_rect)
+
+    textsurface = myfont.render('Press any key to exit', True, (0, 0, 0))
+    text_rect = textsurface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    menu_window.blit(textsurface, text_rect)
+    pygame.display.update()
+    # wait 5s
+    time.sleep(5)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                break
+            if event.type == QUIT:
+                pygame.quit()      
+                sys.exit()
+        if event.type == pygame.KEYDOWN:
+            break
+        FramePerSec.tick(FPS)
